aaf39dd169aca908f459b14e9a525524935e6ee6
jasmithadodda <jasmithadodda@gmail.com>
2024-08-20 14:33:27 +0530

    checking ul and li to be present in the document

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,9 +1,17 @@
-import { render, screen } from "@testing-library/react"
-import Skills from "./Skills"
+import { render, screen } from "@testing-library/react";
+import Skills from "./Skills";
 
-describe("skills",()=>{
-    const skills = ["HTML","CSS","JAVASCRIPT"]
-    render(<Skills skills={skills}/>)
-    const listElement = screen.getByRole("list")
-    expect(listElement).toBeInTheDocument()
-})
+describe("skills", () => {
+    const skills = ["HTML", "CSS", "JAVASCRIPT"];
+    test("renders correctly", () => {
+      render(<Skills skills={skills} />);
+      const listElement = screen.getByRole("list");  //ul
+      expect(listElement).toBeInTheDocument();
+    });
+  
+    test("renders alist of skills", () => {
+      render(<Skills skills={skills} />);
+      const listItemElements = screen.getAllByRole("listitem") //li
+      expect(listItemElements).toHaveLength(skills.length)
+    });
+  });
