9421de7e9f636ec7fa1c3f56989edcf54e1e1e75
jasmithadodda <jasmithadodda@gmail.com>
2024-08-20 15:42:35 +0530

    user-event lib ver update.. button click

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import Counter from "./counter"
-
+import user from '@testing-library/user-event'
 
 describe('counter',()=>{
     test('renders correctly',()=>{
@@ -18,5 +18,17 @@ describe('counter',()=>{
         render(<Counter/>)
         const countElement = screen.getByRole('heading')
         expect(countElement).toHaveTextContent("0")
+     })
+
+     test('incremnts  a count to 1',async () => { 
+        user.setup()
+        render(<Counter/>)
+        const incrementButton = screen.getByRole('button',{
+            name:"Increment"
+        })
+        await user.click(incrementButton)
+        const countElement = screen.getByRole('heading')
+        expect(countElement).toHaveTextContent("1")
+
      })
 })
