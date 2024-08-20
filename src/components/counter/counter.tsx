bad9132c7031a308270e590c4812e6ac065f0754
jasmithadodda <jasmithadodda@gmail.com>
2024-08-20 15:58:28 +0530

    updated counter component with input

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
   return (
     <div>
       <h1>{count}</h1>
@@ -11,6 +12,21 @@ function Counter() {
         }}
       >
         Increment
+      </button>
+      <input
+        type="number"
+        name="amount"
+        value={amount}
+        onChange={(e) => {
+          setAmount(parseInt(e.target.value));
+        }}
+      />
+      <button
+        onClick={() => {
+          setCount(amount);
+        }}
+      >
+        Set
       </button>
     </div>
   );
